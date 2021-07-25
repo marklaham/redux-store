8db0e5b67c49237d269eb0b958f381229c71f1e3
Mark Laham <marklaham@gmail.com>
2021-07-25 09:45:45 -0400

    most of redux added in

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,19 +1,14 @@
-import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers'
+import React from "react";
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+const StoreProvider = ({ ...props }) => {
 
-  return <Provider value={[state, dispatch]} {...props} />;
+  return <Provider store={store} {...props} />;
 };
 
 const useStoreContext = () => {
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -11,7 +11,15 @@ import {
   TOGGLE_CART
 } from "./actions";
 
-export const reducer = (state, action) => {
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+};
+
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
@@ -88,3 +96,7 @@ export const reducer = (state, action) => {
 export function useProductReducer(initialState) {
   return useReducer(reducer, initialState)
 }
+
+
+
+export default reducer;
